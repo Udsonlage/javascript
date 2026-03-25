b4562067d40a3aaca6c0f061a2985adae9537702
Udsonlage <udson.ramos@naimseguro.com.br>
2026-03-25 14:32:59 -0300

    parte de exercício sobre variáveis

---
+++ ex-variavel/script.js
@@ -0,0 +1,11 @@
+//variável é um valor, número ou objeto que definimos com um nome, que são os tipos primitivos.
+
+// O símbolo de "=" significa "recebe".
+
+var numero = 2; //number, não usa aspas
+
+var palavra = "paralelepidedo"; //string, mesmo se colocar um número, a string é definida pelas aspas ""
+
+var objeto = []; //object, definido pelos conchetes
+
+var array = {}; //array, definido pelas chaves
