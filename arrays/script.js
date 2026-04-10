f31afc8518aedad034c5d7aba2a61502ad074f04
Udsonlage <udson.ramos@naimseguro.com.br>
2026-04-10 14:34:53 -0300

    estudo de funções

---
+++ arrays/script.js
@@ -0,0 +1,20 @@
+/*
+    let num = [5, 8, 2, 9, 3]
+
+    num[6] = 5
+
+    num.push(10)
+
+    num.sort()
+
+    console.log(num[0])
+    console.log(`Nosso vetor é o ${num}`)
+    console.log(`O vetor tem ${num.length} posições`)
+    console.log(`Os vetores em ordem são ${num.sort}`)
+*/
+
+let valores = [1, 4, 6, 8, 9]
+
+for(let pos = 0;pos < valores.length;pos++) {
+    console.log(`A posição ${pos} tem o valor de ${valores[pos]}`)
+}
