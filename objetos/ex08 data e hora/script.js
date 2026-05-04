8ad80417e41dbd554ff5c745472877da9c8b3f95
Udsonlage <udson.ramos@naimseguro.com.br>
2026-05-04 08:30:37 -0300

    Estudo de data e hora

---
+++ objetos/ex08 data e hora/script.js
@@ -0,0 +1,27 @@
+const data = new Date()
+
+// console.log(Date.now) // Timestamp (milisegundos desde 1 de janeiro de 1970, 00:00:00 UTC)
+
+console.log(data)
+console.log(`Mês: ${data.getDate()}`)
+
+
+// getDate() = Dia do mês
+// getDay() = Dia da semana (número)
+// getFullYear() = Ano com 4 dígitos
+// getHours() = Horas
+// getMilliseconds() = Milisegundos
+// getMinutes() Minutos
+// getMonth() = Mês
+// getSeconds() = Segundos
+// getTime() = Timestamp (milisegundos desde 1 de janeiro de 1970, 00:00:00 UTC)
+// Date.now() = Timestamp (milisegundos desde 1 de janeiro de 1970, 00:00:00 UTC)
+// getTimezoneOffset() = Timezone da localidade
+// setDate() = Define um dia do mês para a data
+// setMonth() = Define um mês para a data
+// setFullYear() = Define um ano para a data
+// setHours() = Define horas
+// setMinutes() = Define minutos
+// setSeconds() = define segundos
+// setMilliseconds() = Define milisegundos
+// toDateString() = Retorna somente a data
