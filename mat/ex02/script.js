bf34a5eb6ad2b7b397faacc03277df86e3024143
Udsonlage <udson.ramos@naimseguro.com.br>
2026-05-06 14:39:50 -0300

    exercício de olhos com math

---
+++ mat/ex02/script.js
@@ -0,0 +1,15 @@
+const olhos = [...document.getElementsByClassName("olho")]
+
+let posxMouse = 0
+let posyMouse = 0
+
+window.addEventListener("mousemove",(evt)=>{
+    posxMouse = evt.clientX
+    posyMouse = evt.clientY
+
+    const rot = Math.atan2(posyMouse,posxMouse) * 180 / Math.PI
+
+    olhos.map((o)=>{
+        o.style.transform = `rotate(${rot}deg)`
+    })
+})
--- mat/script.js
+++
@@ -1,7 +0,0 @@
-//const mat = document.querySelector("#mat")
-
-//mat.innerHTML = Math.floor(Math.random()*10)
-
-let p = new Promise((a, b)=>{
-    
-})
